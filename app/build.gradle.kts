e1219cd8f3fd34c45d5db41ae118a338edce53ed
Shaheen <mhshaheen.shadhin@gmail.com>
2023-12-19 17:27:36 +0600

    sdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.androidwidgetapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.androidwidgetapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
